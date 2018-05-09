eb0d76856b81de8a103fa0a5b05e860da5e514b3
Jordan Hiatt <jordan.hiatt11@gmail.com>
2018-05-09 11:16:44 -0400

    title annimation working

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+//navbar transitions
+
 $(window).scroll(function(){
 
 	if ( $(document).scrollTop() > 550 ) {			
@@ -19,4 +21,28 @@ $("#work").click(function(e) {
         scrollTop: $("#worksection").offset().top -150
     }, 800);
 });
+
+
+//title with changing words
+var nameAttribute = ["scientists", "designers", "learners"];
+var nameAttribute2 = ["code", "design", "discover", "research"];
+var nameCounter = 0;
+var nameCounter2 = 0;
+
+function changeName() {
+  var nameElement = document.getElementById("name");
+  nameElement.innerHTML = "We are " + nameAttribute[nameCounter] + " who " + nameAttribute2[nameCounter2];
+  if (nameCounter >= (nameAttribute.length - 1)) {
+    nameCounter = 0;
+  } else {
+    nameCounter += 1;
+  }
+  if (nameCounter2 >= (nameAttribute2.length - 1)) {
+    nameCounter2 = 0;
+  } else {
+    nameCounter2 += 1;
+  }
+}
+
+window.setInterval(changeName, 2000);
 
